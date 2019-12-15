7e552fc351d4b6a855403e41b1c523d964568ac1
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2019-12-15 13:49:52 +0100

    Update build process (stop tracking output files)

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
-const Critters = require('critters-webpack-plugin');
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
+const Critters = require('critters-webpack-plugin')
 
 module.exports = {
   entry: __dirname + '/src/index.js',
 
   output: {
-    path: __dirname,
-    filename: 'bundle.js'
+    path: __dirname + '/dist',
+    filename: 'main.js',
   },
 
   module: {
@@ -19,15 +19,27 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env']
-          }
-        }
+            presets: ['env'],
+          },
+        },
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            },
+          },
+        ],
+      },
+    ],
   },
 
   plugins: [
@@ -41,11 +53,11 @@ module.exports = {
         removeRedundantAttributes: true,
         removeScriptTypeAttributes: true,
         useShortDoctype: true,
-      }
+      },
     }),
     new HtmlWebpackInlineSourcePlugin(),
     new Critters({
       preload: 'body',
     }),
-  ]
+  ],
 }
